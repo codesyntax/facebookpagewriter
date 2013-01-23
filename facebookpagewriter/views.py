962a73b77e42172a9a308c138410744a60b1a11f
Aitzol Naberan <anaberan@codesyntax.com>
2013-01-23 09:30:43 +0000

    Use facebook-sdk to get the access_token Clean expiration related code

--- facebookpagewriter/views.py
+++ facebookpagewriter/views.py
@@ -1,15 +1,14 @@
 import hashlib
 import random
 import string
-import urllib2
+
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.conf import settings
 from django.contrib.sites.models import get_current_site
-
+from facebook import get_access_token_from_code
 from facebookpagewriter.models import FacebookConfig
-import datetime
 
 APP_ID = getattr(settings, 'FB_APP_ID')
 APP_SECRET = getattr(settings, 'FB_APP_SECRET')
@@ -21,22 +20,10 @@ def fblogin(request):
         dialog_url = "https://www.facebook.com/dialog/oauth?client_id=%(id)s&redirect_uri=http://%(site)s%(url)s&state=%(state)s&scope=publish_actions,status_update,publish_stream,manage_pages" % {'id':APP_ID,'state': state, 'url': reverse('fblogin'),'site':current_site.domain}
         return HttpResponseRedirect(dialog_url)
     else:
-        #state = request.GET['state']
         code = request.GET['code']
-        access_token_url = 'https://graph.facebook.com/oauth/access_token?client_id=%(id)s&redirect_uri=http://%(site)s%(url)s&client_secret=%(secret)s&code=%(code)s'%{'id':APP_ID, 'secret':APP_SECRET, 'code':code, 'url': reverse('fblogin'),'site':current_site.domain}
-        response = urllib2.urlopen(access_token_url)       
-        html = response.read()
-        parts = html.split('&')
-        if len(parts) == 1:
-            token = parts[0].split('=')[1]
-            expiration = 50000
-        elif len(parts) == 2:
-            token = parts[0].split('=')[1]
-            expiration = parts[1].split('=')[1]            
+        result = get_access_token_from_code(code, 'http://%(site)s%(url)s' % {'site': current_site.domain, 'url': reverse('fblogin')}, APP_ID, APP_SECRET)
+        token = result.get('access_token','')
         config, created = FacebookConfig.objects.get_or_create(app_id=APP_ID,app_secret=APP_SECRET)
         config.access_token = token
-        config.access_token_expiration = expiration
-        config.updated_at = datetime.datetime.now()
         config.save()
-        #graph.get_object('AitzolTest',fields="access_token")
-        return HttpResponse('token: %(token)s expiration: %(expiration)s'% {'expiration': expiration, 'token': token})
+        return HttpResponse('token: %(token)s'% {'token': token})
