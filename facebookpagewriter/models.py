773e1bc1dee21b3fb3e8ed6055f804a50c43c3c2
Aitzol Naberan <anaberan@codesyntax.com>
2013-01-23 09:27:35 +0000

    This fields are not required because de flow of token renovation at Facebook: https://developers.facebook.com/blog/post/2011/05/13/how-to--handle-expired-access-tokens/

--- facebookpagewriter/models.py
+++ facebookpagewriter/models.py
@@ -4,6 +4,4 @@ class FacebookConfig(models.Model):
     app_id = models.CharField(max_length=40)
     app_secret = models.CharField(max_length=120)
     access_token = models.CharField(max_length=255, blank=True, null=True)
-    access_token_expiration = models.PositiveIntegerField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
     
